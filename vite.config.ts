5dcb125d003c34a05732d3f44c5c3d34d68b9812
jonorl <39122230+jonorl@users.noreply.github.com>
2026-02-26 19:38:51 +0300

    Fix: both legacy and new route works now

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/voice-clone/',
+  base: './',
   plugins: [react(), tailwindcss()],
 })
